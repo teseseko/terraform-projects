eb6bc2845ef26e34a20ddf49cd37e32d5170995c
Tese Ahamioje <teseseko@gmail.com>
2023-03-18 20:55:24 -0400

    created sns file

--- variable.tf
+++ variable.tf
@@ -79,3 +79,10 @@ variable "ssl_certificate_arn" {
   description   = "ssl certificate arn"
   type          = string
 }
+
+# sns topic variable
+variable "operator_email" {
+  default       = "james.ahamioje@gmail.com"
+  description   = "a valid email address"
+  type          = string
+}
